33fe5be79c6d1c89cb1056f3c983f33cc80fcba5
Adrien Pessu <7055334+adrienpessu@users.noreply.github.com>
2022-10-11 09:40:45 +0200

    chore: ci

---
+++ frontend/cypress/e2e/spec.cy.ts
@@ -0,0 +1,30 @@
+Cypress.env()
+
+
+describe('Nominal test', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:4200');
+
+    cy.get('body').then($body => {
+      if ($body.find('button#login').length) {
+        cy.get('button#login').click();
+
+        cy.url({timeout: 35000}).then(url => {
+          if (url.indexOf(".eu.auth0.com") > -1) {
+            cy.url({timeout: 35000}).should('contain', 'state=');
+            cy.get('#username')
+              .invoke('attr', 'value', Cypress.env('auth_username'),)
+              .should('have.attr', 'value', Cypress.env('auth_username'),);
+            cy.get('#password').invoke('attr', 'value', Cypress.env('auth_password'),)
+              .should('have.attr', 'value', Cypress.env('auth_password'),)
+            cy.get('button[type=submit][value=default]').click()
+            cy.url({timeout: 35000}).should('contain', '4200')
+          }
+        });
+        debugger;
+        cy.get('div#greatings', {timeout: 35000})
+          .should('have.text', Cypress.env('auth_username'));
+      }
+    });
+  })
+})
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {AuthService} from "@auth0/auth0-angular";
+import {take} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -8,9 +10,15 @@ import {Router} from "@angular/router";
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(public auth: AuthService, private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.auth.isAuthenticated$.pipe(take(1)).subscribe(result => {
+      if (result) {
+        this.goToASafePlace();
+      }
+    });
   }
 
   goToASafePlace() {
